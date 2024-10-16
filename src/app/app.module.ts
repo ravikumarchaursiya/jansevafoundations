37da877c7d7b5c7a91bf8ab3b184a8ada356084e
ravi kumar chaursiya <rchaursiya383@gmail.com>
2024-10-16 17:02:16 +0530

    validation-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
 
   ],
   providers: [],
--- src/app/website/pages/login/login.component.ts
+++ src/app/website/pages/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+loginForm:FormGroup
+email:any = 'rchauirsiya383@gmail.com'
+password:any = '123456789'
+  constructor(private fb:FormBuilder,private router:Router,private toastr:ToastrService) { }
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:[],
+      password:[]
+    })
   }
-
+  onSubmit(){
+    if(this.loginForm.valid){
+      debugger
+      const obj = this.loginForm.getRawValue()
+      if(obj.email === this.email && obj.password === this.password){
+        this.router.navigateByUrl('/dashboard')
+      }else{
+        this.toastr.error("Inavlid Email Id & password")
+      }
+    
+    }else{
+      
+    }
+  }
 }
