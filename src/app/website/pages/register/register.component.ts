a2cf7238201fece33d43fb2e71c83999303c3250
ravi <ravi.chaurasia@indicsoft.com>
2024-10-25 19:02:36 +0530

    changes-added

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { WebsiteService } from '../../Services/website.service';
+import { Router, RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,8 @@ export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
   roles:any 
   baseUrl:any = '/UserRegistration/register'
-  constructor(private fb: FormBuilder,private toastr:ToastrService,private registerService:WebsiteService) {
+  constructor(private fb: FormBuilder,private toastr:ToastrService,
+    private registerService:WebsiteService,private router:Router) {
     this.roles  = ['Donor', 'Volunteer', 'Admin', 'Member'];
   }
 
@@ -21,7 +23,8 @@ export class RegisterComponent implements OnInit {
       name: ['', [Validators.required, Validators.minLength(3)]],
       emailAddress: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      role: ['', Validators.required]
+      role: ['', Validators.required],
+      MobileNumber:[]
     });
   }
 
@@ -29,8 +32,11 @@ export class RegisterComponent implements OnInit {
     if (this.registerForm.valid) {
        const data = this.registerForm.value
       this.registerService.userRegisteration(this.baseUrl,data).subscribe(res=>{
-        if(res.status==='200'){
+        if(res?.message){
           this.toastr.success('You Have Register Successfully')
+          this.router.navigateByUrl('/login')
+        }else{
+          this.toastr.error(res?.message)
         }
       })
       // Call your API to register the user
