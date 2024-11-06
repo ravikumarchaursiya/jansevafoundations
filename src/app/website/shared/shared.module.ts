dd71e8f83b34e50d6f620e826bc53b4b707f9eba
ravi kumar chaursiya <rchaursiya383@gmail.com>
2024-11-06 15:49:09 +0530

    changes-added

--- src/app/website/shared/shared.module.ts
+++ src/app/website/shared/shared.module.ts
@@ -4,19 +4,28 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { DynamicModule } from '../dynamic-component/dynamic.module';
+import { CharSpaceOnlyDirective } from 'src/app/directives/charspaceonly.directive';
+import { DecimalDirective } from 'src/app/directives/decimal.directive';
+import { EmailValidatorDirective } from 'src/app/directives/emailvalidator.directive';
+import { NumericOnlyDirective } from 'src/app/directives/numeric-only.directive';
 
 @NgModule({
   declarations: [
     BannerComponent,
-  
+    CharSpaceOnlyDirective,
+    DecimalDirective,
+    EmailValidatorDirective,
+    NumericOnlyDirective
+
   ],
   imports: [
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
-    DynamicModule
+    DynamicModule,
+    
   ],
-  exports: [BannerComponent,DynamicModule],
+  exports: [BannerComponent,DynamicModule,CharSpaceOnlyDirective,DecimalDirective,EmailValidatorDirective,NumericOnlyDirective],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
