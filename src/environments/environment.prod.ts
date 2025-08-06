9a093eb3d1a27dd3e3ed942efb17aa8b539aae27
ravi kumar chaursiya <rchaursiya383@gmail.com>
2025-08-06 17:31:56 +0530

    sdfsad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 const apiUrl = 'http://13.201.123.93/api'
 export const environment = {
   production: true,
-  // strapiUrl: 'http://localhost:1337/api',
+  strapiUrl: 'http://localhost:1337/api',
   config: {apiUrl:`${apiUrl}`}
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,6 +3,6 @@
 const apiUrl = 'http://13.201.123.93/api'
 export const environment = {
   production: false,
-  // strapiUrl: 'http://localhost:1337/api',
+  strapiUrl: 'http://localhost:1337/api',
   config: {apiUrl:`${apiUrl}`}
 };
